6ce29cedad5551c328d82d045509d58356327bbc
pbmacintyre <pbmacintyre@gmail.com>
2025-11-06 16:21:20 -0400

    code tested and cleaned up

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (C) 2019-2024 Paladin Business Solutions
+ * Copyright (C) 2019-2025 Paladin Business Solutions
  */
 ob_start();
 session_start();
@@ -12,7 +12,7 @@ show_errors();
 
 page_header(0);
 
-function show_form($message, $label = "", $print_again = false) {
+function show_form($message, $print_again = false) {
 
     ?>
     <form action="" method="post" enctype="multipart/form-data">
@@ -22,7 +22,7 @@ function show_form($message, $label = "", $print_again = false) {
                     <img src="images/rc-logo.png"/>
                     <h2><?php echo app_name(); ?></h2>
                     <?php
-                    if ($print_again == true) {
+                    if ($print_again) {
                         echo "<p class='msg_bad'>" . $message . "</strong></font>";
                     } else {
                         echo "<p class='msg_good'>" . $message . "</p>";
@@ -35,7 +35,7 @@ function show_form($message, $label = "", $print_again = false) {
                     <p style='display: inline;'>1st Receiving SMS #:</p>
                 </td>
                 <td class="right_col">
-                    <input type="text" name="to_sms_number_1">
+                    <input type="text" name="to_sms_numbers[]">
                 </td>
             </tr>
             <tr class="CustomTable">
@@ -43,7 +43,7 @@ function show_form($message, $label = "", $print_again = false) {
                     <p style='display: inline;'>2nd Receiving SMS #:</p>
                 </td>
                 <td class="right_col">
-                    <input type="text" name="to_sms_number_2">
+                    <input type="text" name="to_sms_numbers[]">
                 </td>
             </tr>
             <tr class="CustomTable">
@@ -51,7 +51,7 @@ function show_form($message, $label = "", $print_again = false) {
                     <p style='display: inline;'>3rd Receiving SMS #:</p>
                 </td>
                 <td class="right_col">
-                    <input type="text" name="to_sms_number_3">
+                    <input type="text" name="to_sms_numbers[]">
                 </td>
             </tr>
             <tr class="CustomTable">
@@ -59,7 +59,7 @@ function show_form($message, $label = "", $print_again = false) {
                     <p style='display: inline;'>SMS Message:</p>
                 </td>
                 <td class="right_col">
-                    <textarea name="sms_message"></textarea>
+                    <textarea name="sms_message"><?php if ($print_again) { echo strip_tags($_POST['sms_message']); } ?></textarea>
                 </td>
             </tr>
             <tr class="CustomTable">
@@ -78,97 +78,60 @@ function show_form($message, $label = "", $print_again = false) {
     <?php
 }
 
-function check_sms_form() {
+function check_form() {
     show_errors();
 
     $print_again = false;
-    $label = "";
     $message = "";
 
     /* ============================================ */
     /* ====== START data integrity checks ========= */
     /* ============================================ */
 
-    $to_sms_number = strip_tags($_POST['to_sms_number']);
+//    $to_sms_number_2 = strip_tags($_POST['to_sms_number_2']);
+//    $to_sms_number_3 = strip_tags($_POST['to_sms_number_3']);
     $sms_message = strip_tags($_POST['sms_message']);
 
     if ($sms_message == "") {
         $print_again = true;
         $message = "No SMS message body has been provided";
     }
-    if ($to_sms_number == "") {
+
+    $all_blank = empty(array_filter($_POST['to_sms_numbers'], function($v) {
+        return trim($v) !== '';
+    }));
+
+    if ($all_blank) {
         $print_again = true;
-        $message = "No receiving SMS Number has been provided";
+        $message = "No receiving SMS Numbers have been provided";
+    } else {
+        $to_sms_numbers = [];
+        foreach ($_POST['to_sms_numbers'] as $number) {
+            if ($number != '') {
+                $to_sms_numbers[] = ['phoneNumber' => $number];
+            }
+        }
     }
 
     /* ========================================== */
     /* ====== END data integrity checks ========= */
     /* ========================================== */
     if ($print_again) {
-        show_form($message);
+        show_form($message, $print_again);
     } else {
-        send_sms($to_sms_number, $sms_message);
+        send_sms($to_sms_numbers, $sms_message);
         $message = "SMS message has been sent Successfully";
         show_form($message);
     }
 
 }
 
-function check_fax_form() {
-    show_errors();
-
-    $print_again = false;
-    $label = "";
-    $message = "";
-
-    /* ============================================ */
-    /* ====== START data integrity checks ========= */
-    /* ============================================ */
-
-    $to_fax_number = strip_tags($_POST['to_fax_number']);
-    $cover_note = strip_tags($_POST['cover_note']);
-    $target_file = basename($_FILES["file_to_fax"]["name"]);
-
-    if ($target_file == "") {
-        $print_again = true;
-        $label = "";
-        $message = "No file selected to be uploaded";
-    }
-    if ($cover_note == "") {
-        $print_again = true;
-        $label = "cover_note";
-        $message = "No cover note has been provided";
-    }
-    if ($to_fax_number == "") {
-        $print_again = true;
-        $label = "to_fax_number";
-        $message = "No receiving Fax Number has been provided";
-    }
-
-    /* ========================================== */
-    /* ====== END data integrity checks ========= */
-    /* ========================================== */
-
-    $file_with_path = upload_file();
-
-    $fax_sent_id = send_fax($to_fax_number, $file_with_path, $cover_note);
-    if ($fax_sent_id > 0) {
-        $print_again = true;
-        $label = "";
-        $message = "Fax sent successfully (Sent id): " . $fax_sent_id;
-        // clean out the file
-        unlink($file_with_path);
-    }
-    show_form($message, $label, $print_again);
-}
 
 /* ============= */
 /*  --- MAIN --- */
 /* ============= */
 if (isset($_POST['send_sms'])) {
-    check_sms_form();
-} elseif (isset($_POST['send_fax'])) {
-    check_fax_form();
+    check_form();
 } else {
     $message = "Please provide the needed information. <br/><br/>";
     show_form($message);
